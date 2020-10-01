37c3e3c381b8bfbb67ad51b280e751a8bf5b0dcf
Kevin Frei <kevinfrei@hotmail.com>
2020-10-01 16:49:02 -0700

    A little more FTON testing

--- src/__tests__/fton.ts
+++ src/__tests__/fton.ts
@@ -3,3 +3,22 @@ import * as FTON from '../FTON';
 test('FTON sanity', () => {
   FTON.stringify([]);
 });
+
+test('FTON set roundtrip', () => {
+  const set = new Set<string>(['a', 'b']);
+  const setString = FTON.stringify(set);
+  const newSet = FTON.parse(setString);
+  const next = FTON.stringify(newSet);
+  expect(next).toEqual(setString);
+});
+
+test('FTON map roundtrip', () => {
+  const map = new Map<string, string>([
+    ['a', 'b'],
+    ['c', 'd'],
+  ]);
+  const mapString = FTON.stringify(map);
+  const newMap = FTON.parse(mapString);
+  const next = FTON.stringify(newMap);
+  expect(next).toEqual(mapString);
+});
