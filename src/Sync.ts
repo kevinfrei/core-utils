298ff40376c8a693dcc6b89df053aee7bc0acf69
Kevin Frei <kevinfrei@hotmail.com>
2021-04-28 21:57:46 -0700

    Added a couple of debouncers

--- src/Sync.ts
+++ src/Sync.ts
@@ -41,3 +41,45 @@ export async function MaybeWait<T>(func: () => Promise<T> | T): Promise<T> {
     return res;
   }
 }
+
+/**
+ * This invokes func no *sooner* than `timeout` milliseconds in the future, but
+ * will restarts the timer every time the function is invoked, so if you call it
+ * every timeout-1 milliseconds, it will never invoke the function
+ * @param  {()=>void} func
+ * @param  {number} timeout
+ */
+export function DebouncedDelay(func: () => void, timeout: number): () => void {
+  let debounceTimer: number | NodeJS.Timer | null = null;
+  function ping() {
+    if (debounceTimer !== null) {
+      clearTimeout(debounceTimer as any);
+    }
+    debounceTimer = setTimeout(() => {
+      debounceTimer = null;
+      func();
+    }, timeout);
+  }
+  return ping;
+}
+
+/**
+ * This invokes func every `timeout` milliseconds in the future, so if you call
+ * it before the timer has completed, it does nothing. Logically, it "buffers"
+ * invocations, flushing the buffer every X ms.
+ * @param  {()=>void} func
+ * @param  {number} timeout
+ */
+export function DebouncedEvery(func: () => void, timeout: number): () => void {
+  let debounceTimer: number | NodeJS.Timer | null = null;
+  function ping() {
+    if (debounceTimer !== null) {
+      return;
+    }
+    debounceTimer = setTimeout(() => {
+      debounceTimer = null;
+      func();
+    }, timeout);
+  }
+  return ping;
+}
